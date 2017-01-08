34533aba718b4ca5cb70aae44dfab95ec4d1c5c1
ShadowBeta <huefield12345@gmail.com>
2017-01-08 15:23:14 -0200

    Add files via upload

---
+++ conexao_bd.php
@@ -0,0 +1,10 @@
+<?php  
+
+	$servidor = "sql207.epizy.com";
+	$usuario = "epiz_19121077";
+	$senha = "InFsite7812";
+	$dbname = "epiz_19121077_contatobd";
+
+	// Criar a conexão
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+?>
